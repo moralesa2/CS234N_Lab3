5ab4c8f37bb643ba0306a3d6c9876e3ca984e3d1
Aubrey M <moralesa2@my.lanecc.edu>
2023-10-28 14:50:47 -0700

    CustomerProps & tests complete, .json files ignored

--- MMABooksProps/CustomerProps.cs
+++ MMABooksProps/CustomerProps.cs
@@ -2,9 +2,67 @@
 using System.Collections.Generic;
 using System.Text;
 
+using MMABooksTools;
+using DBDataReader = MySql.Data.MySqlClient.MySqlDataReader;
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 namespace MMABooksProps
 {
-    public class CustomerProps
+    [Serializable()]
+    public class CustomerProps : IBaseProps
     {
+        //Customer properties
+        public int CustomerID { get; set; } = 0;
+        public string Name { get; set; } = "";
+        public string Address { get; set; } = "";
+        public string City { get; set; } = "";
+        public string State { get; set; } = "";
+        public string Zipcode { get; set; } = "";
+        public int ConcurrencyID { get; set; } = 0;
+
+        public object Clone()
+        {
+            CustomerProps p = new CustomerProps();
+            p.CustomerID = this.CustomerID;
+            p.Name = this.Name;
+            p.Address = this.Address;
+            p.City = this.City;
+            p.State = this.State;
+            p.Zipcode = this.Zipcode;
+            p.ConcurrencyID = this.ConcurrencyID;
+            return p;
+        }
+
+        public string GetState()
+        {
+            string jsonString;
+            jsonString = JsonSerializer.Serialize(this);
+            return jsonString;
+        }
+
+        public void SetState(string jsonString)
+        {
+            CustomerProps p = JsonSerializer.Deserialize<CustomerProps>(jsonString);
+            this.CustomerID = p.CustomerID;
+            this.Name = p.Name;
+            this.Address = p.Address;
+            this.City = p.City;
+            this.State = p.State;
+            this.Zipcode = p.Zipcode;
+            this.ConcurrencyID = p.ConcurrencyID;
+        }
+
+        public void SetState(DBDataReader dr)
+        {
+            this.CustomerID = (int)dr["CustomerID"];
+            this.Name = (string)dr["Name"];
+            this.Address = (string)dr["Address"];
+            this.City = (string)dr["City"];
+            this.State = (string)dr["State"];
+            this.Zipcode = (string)dr["ZipCode"];
+            this.ConcurrencyID = (Int32)dr["ConcurrencyID"];
+        }
     }
 }
---
+++ MMABooksTests/CustomerPropsTests.cs
@@ -0,0 +1,53 @@
+using NUnit.Framework;
+
+using MMABooksProps;
+using System;
+
+namespace MMABooksTests
+{
+    [TestFixture]
+    public class CustomerPropsTests
+    {
+        CustomerProps props;
+        [SetUp]
+        public void Setup()
+        {
+            props = new CustomerProps();
+            props.CustomerID = 1;
+            props.Name = "Test Name";
+            props.Address = "101 Test Address";
+            props.City = "Testcity";
+            props.State = "TS";
+            props.Zipcode = "10001";
+        }
+
+        [Test]
+        public void TestGetState()
+        {
+            string jsonString = props.GetState();
+            Console.WriteLine(jsonString);
+            Assert.IsTrue(jsonString.Contains(props.Address));
+            Assert.IsTrue(jsonString.Contains(props.Name));
+        }
+
+        [Test]
+        public void TestSetState()
+        {
+            string jsonString = props.GetState();
+            CustomerProps newProps = new CustomerProps();
+            newProps.SetState(jsonString);
+            Assert.AreEqual(props.CustomerID, newProps.CustomerID);
+            Assert.AreEqual(props.Name, newProps.Name);
+            Assert.AreEqual(props.ConcurrencyID, newProps.ConcurrencyID);
+        }
+
+        [Test]
+        public void TestClone()
+        {
+            CustomerProps newProps = (CustomerProps)props.Clone();
+            Assert.AreEqual(props.CustomerID, newProps.CustomerID);
+            Assert.AreEqual(props.Name, newProps.Name);
+            Assert.AreEqual(props.ConcurrencyID, newProps.ConcurrencyID);
+        }
+    }
+}
