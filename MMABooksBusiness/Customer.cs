0ddf2b756cc1f21e4f353bd308842e3ffa3c82a8
Aubrey M <moralesa2@my.lanecc.edu>
2023-10-29 15:26:19 -0700

    Customer classs and tests complete

--- MMABooksBusiness/Customer.cs
+++ MMABooksBusiness/Customer.cs
@@ -1,10 +1,205 @@
 ﻿using System;
+
+using MMABooksTools;
+using MMABooksProps;
+using MMABooksDB;
+
 using System.Collections.Generic;
-using System.Text;
 
 namespace MMABooksBusiness
 {
-    public class Customer
+    public class Customer : BaseBusiness
     {
+        #region constructors
+
+        public Customer() : base()
+        {
+        }
+
+        public Customer(int key) : base(key)
+        {
+        }
+
+        private Customer(CustomerProps p) : base(p)
+        {
+        }
+
+        #endregion
+
+        public int CustomerID
+        {
+            get
+            {
+                return ((CustomerProps) mProps).CustomerID;
+            }
+        }
+
+        public String Name
+        {
+            get
+            {
+                return ((CustomerProps)mProps).Name;
+            }
+
+            set
+            {
+                if (!(value == ((CustomerProps)mProps).Name))
+                {
+                    if (value.Trim().Length >= 1 && value.Trim().Length <= 100)
+                    {
+                        mRules.RuleBroken("Name", false);
+                        ((CustomerProps)mProps).Name = value;
+                        mIsDirty = true;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Name must be no more than 100 characters long.");
+                    }
+                }
+            }
+        }
+
+        public String Address
+        {
+            get
+            {
+                return ((CustomerProps)mProps).Address;
+            }
+
+            set
+            {
+                if (!(value == ((CustomerProps)mProps).Address))
+                {
+                    if (value.Trim().Length >= 1 && value.Trim().Length <= 50)
+                    {
+                        mRules.RuleBroken("Address", false);
+                        ((CustomerProps)mProps).Address = value;
+                        mIsDirty = true;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Address must be no more than 50 characters long.");
+                    }
+                }
+            }
+        }
+
+        public String City
+        {
+            get
+            {
+                return ((CustomerProps)mProps).City;
+            }
+
+            set
+            {
+                if (!(value == ((CustomerProps)mProps).City))
+                {
+                    if (value.Trim().Length >= 1 && value.Trim().Length <= 20)
+                    {
+                        mRules.RuleBroken("City", false);
+                        ((CustomerProps)mProps).City = value;
+                        mIsDirty = true;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("City must be no more than 20 characters long.");
+                    }
+                }
+            }
+        }
+
+        public String State
+        {
+            get
+            {
+                return ((CustomerProps)mProps).State;
+            }
+
+            set
+            {
+                if (!(value == ((CustomerProps)mProps).State))
+                {
+                    if (value.Trim().Length >= 1 && value.Trim().Length <= 2)
+                    {
+                        mRules.RuleBroken("State", false);
+                        ((CustomerProps)mProps).State = value;
+                        mIsDirty = true;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("State must be no more than 2 characters long.");
+                    }
+                }
+            }
+        }
+
+        public String Zipcode
+        {
+            get
+            {
+                return ((CustomerProps)mProps).Zipcode;
+            }
+
+            set
+            {
+                if (!(value == ((CustomerProps)mProps).Zipcode))
+                {
+                    if (value.Trim().Length >= 1 && value.Trim().Length <= 15)
+                    {
+                        mRules.RuleBroken("Zipcode", false);
+                        ((CustomerProps)mProps).Zipcode = value;
+                        mIsDirty = true;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Zipcode must be no more than 15 characters long.");
+                    }
+                }
+            }
+        }
+
+        public override object GetList()
+        {
+            List<Customer> customers = new List<Customer>();
+            List<CustomerProps> props = new List<CustomerProps>();
+
+
+            props = (List<CustomerProps>)mdbReadable.RetrieveAll();
+            foreach (CustomerProps prop in props)
+            {
+                Customer c = new Customer(prop);
+                customers.Add(c);
+            }
+
+            return customers;
+        }
+
+        protected override void SetDefaultProperties()
+        {
+        }
+
+        protected override void SetRequiredRules()
+        {
+            mRules.RuleBroken("Name", true);
+            mRules.RuleBroken("Address", true);
+            mRules.RuleBroken("City", true);
+            mRules.RuleBroken("State", true);
+            mRules.RuleBroken("Zipcode", true);
+        }
+
+        protected override void SetUp()
+        {
+            mProps = new CustomerProps();
+            mOldProps = new CustomerProps();
+
+            mdbReadable = new CustomerDB();
+            mdbWriteable = new CustomerDB();
+        }
     }
 }
---
+++ MMABooksTests/CustomerTests.cs
@@ -0,0 +1,144 @@
+﻿using NUnit.Framework;
+
+using MMABooksBusiness;
+using MMABooksProps;
+using MMABooksDB;
+
+using DBCommand = MySql.Data.MySqlClient.MySqlCommand;
+using System.Data;
+
+using System.Collections.Generic;
+using System;
+using MySql.Data.MySqlClient;
+
+namespace MMABooksTests
+{
+    [TestFixture]
+    internal class CustomerTests
+    {
+        [SetUp]
+        public void TestResetDatabase()
+        {
+            CustomerDB db = new CustomerDB();
+            DBCommand command = new DBCommand();
+            command.CommandText = "usp_testingResetData";
+            command.CommandType = CommandType.StoredProcedure;
+            db.RunNonQueryProcedure(command);
+        }
+
+        [Test]
+        public void TestNewCustomerConstructor()
+        {
+            Customer c = new Customer();
+            Assert.AreEqual(string.Empty, c.Address);
+            Assert.AreEqual(string.Empty, c.Name);
+            Assert.IsTrue(c.IsNew);
+            Assert.IsFalse(c.IsValid);
+        }
+
+        
+        [Test]
+        public void TestRetrieveFromDataStoreContructor()
+        {
+            // retrieves from Data Store
+            Customer c = new Customer(1);
+            Assert.IsTrue(c.Name.Length > 0);
+            Assert.AreEqual("Molunguri, A", c.Name);
+            Assert.AreEqual("1108 Johanna Bay Drive", c.Address);
+            Assert.IsFalse(c.IsNew);
+            Assert.IsTrue(c.IsValid);
+        }
+
+        [Test]
+        public void TestSaveToDataStore()
+        {
+            Customer c = new Customer();
+            c.Name = "Tobe Deleted";
+            c.Address = "101 Test Delete";
+            c.City = "Deletecity";
+            c.State = "OR";
+            c.Zipcode = "10001";
+            c.Save();
+            Customer c2 = new Customer(c.CustomerID);
+            Assert.AreEqual(c2.Address, c.Address);
+            Assert.AreEqual(c2.Name, c.Name);
+        }
+
+        [Test]
+        public void TestUpdate()
+        {
+            Customer c = new Customer(1);
+            string oldName = c.Name;
+            string oldAddress = c.Address;
+            c.Name = "Doe, John";
+            c.Address = "101 Test Update";
+            c.Save();
+
+            Assert.AreEqual("Doe, John", c.Name);
+            Assert.AreEqual("101 Test Update", c.Address);
+
+            //Cleanup
+            c.Name = oldName;
+            c.Address = oldAddress;
+            c.Save();
+        }
+
+        [Test]
+        public void TestDelete()
+        {
+            Customer c = new Customer();
+            c.Name = "Tobe Deleted";
+            c.Address = "101 Test Delete";
+            c.City = "Deletecity";
+            c.State = "OR";
+            c.Zipcode = "10001";
+
+            c.Delete();
+            c.Save();
+            Assert.Throws<Exception>(() => new Customer(c.CustomerID));
+        }
+
+
+        [Test]
+        public void TestGetList()
+        {
+            Customer c = new Customer();
+            List<Customer> customers = (List<Customer>)c.GetList();
+            Assert.AreEqual(696, customers.Count);
+            Assert.AreEqual("Abeyatunge, Derek", customers[0].Name);
+            Assert.AreEqual(157, customers[0].CustomerID);
+        }
+
+
+        [Test]
+        public void TestNoRequiredPropertiesNotSet()
+        {
+            // not in Data Store - abbreviation and name must be provided
+            Customer c = new Customer();
+            Assert.Throws<Exception>(() => c.Save());
+        }
+
+
+        [Test]
+        public void TestSomeRequiredPropertiesNotSet()
+        {
+            // not in Data Store - abbreviation and name must be provided
+            Customer c = new Customer();
+            Assert.Throws<Exception>(() => c.Save());
+            c.Name = "Test Name";
+            Assert.Throws<Exception>(() => c.Save());
+        }
+
+        [Test]
+        public void TestInvalidPropertySet()
+        {
+            Customer c = new Customer();
+            Assert.Throws<ArgumentOutOfRangeException>(() => c.Name = "01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890");
+            Assert.Throws<ArgumentOutOfRangeException>(() => c.Address = "012345678901234567890123456789012345678901234567890");
+            Assert.Throws<ArgumentOutOfRangeException>(() => c.City = "012345678901234567890");
+            Assert.Throws<ArgumentOutOfRangeException>(() => c.State = "ORE");
+            Assert.Throws<ArgumentOutOfRangeException>(() => c.Zipcode = "0123456789012345");
+
+        }
+    }
+}
