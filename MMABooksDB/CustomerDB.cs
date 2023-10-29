a5a24e8249ebc7396a2df5897ef26969ac28beed
Aubrey M <moralesa2@my.lanecc.edu>
2023-10-29 12:34:58 -0700

    CustomerDB class and test complete

--- MMABooksDB/CustomerDB.cs
+++ MMABooksDB/CustomerDB.cs
@@ -19,9 +19,8 @@ using DBDbType = MySql.Data.MySqlClient.MySqlDbType;
 
 namespace MMABooksDB
 {
-    public class CustomerDB
+    public class CustomerDB : DBBase, IReadDB, IWriteDB
     {
-        /*
         public IBaseProps Create(IBaseProps p)
         {
             int rowsAffected = 0;
@@ -32,10 +31,18 @@ namespace MMABooksDB
             command.CommandType = CommandType.StoredProcedure;
             command.Parameters.Add("custId", DBDbType.Int32);
             command.Parameters.Add("name_p", DBDbType.VarChar);
-            ... there are more parameters here
+            command.Parameters.Add("address_p", DBDbType.VarChar);
+            command.Parameters.Add("city_p", DBDbType.VarChar);
+            command.Parameters.Add("state_p", DBDbType.VarChar);
+            command.Parameters.Add("zipcode_p", DBDbType.VarChar);
+            //... there are more parameters here
             command.Parameters[0].Direction = ParameterDirection.Output;
             command.Parameters["name_p"].Value = props.Name;
-            ... and more values here
+            command.Parameters["address_p"].Value = props.Address;
+            command.Parameters["city_p"].Value = props.City;
+            command.Parameters["state_p"].Value = props.State;
+            command.Parameters["zipcode_p"].Value = props.Zipcode;
+            //... and more values here
 
             try
             {
@@ -60,6 +67,159 @@ namespace MMABooksDB
                     mConnection.Close();
             }
         }
-         */
+
+        public bool Delete(IBaseProps p)
+        {
+            CustomerProps props = (CustomerProps)p;
+            int rowsAffected = 0;
+
+            DBCommand command = new DBCommand();
+            command.CommandText = "usp_CustomerDelete";
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("custID", DBDbType.Int32);
+            command.Parameters.Add("conCurrId", DBDbType.Int32);
+            command.Parameters["custID"].Value = props.CustomerID;
+            command.Parameters["conCurrId"].Value = props.ConcurrencyID;
+
+            try
+            {
+                rowsAffected = RunNonQueryProcedure(command);
+                if (rowsAffected == 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    string message = "Record cannot be deleted. It has been edited by another user.";
+                    throw new Exception(message);
+                }
+
+            }
+            catch (Exception e)
+            {
+                // log this exception
+                throw;
+            }
+            finally
+            {
+                if (mConnection.State == ConnectionState.Open)
+                    mConnection.Close();
+            }
+        }
+
+        public IBaseProps Retrieve(object key)
+        {
+            DBDataReader data = null;
+            CustomerProps props = new CustomerProps();
+            DBCommand command = new DBCommand();
+
+            command.CommandText = "usp_CustomerSelect";
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("CustID", DBDbType.Int32);
+            command.Parameters["CustID"].Value = (int)key;
+
+            try
+            {
+                data = RunProcedure(command);
+                if (!data.IsClosed)
+                {
+                    if (data.Read())
+                    {
+                        props.SetState(data);
+                    }
+                    else
+                        throw new Exception("Record does not exist in the database.");
+                }
+                return props;
+            }
+            catch (Exception e)
+            {
+                // log this exception
+                throw;
+            }
+            finally
+            {
+                if (data != null)
+                {
+                    if (!data.IsClosed)
+                        data.Close();
+                }
+            }
+        }
+
+        public object RetrieveAll()
+        {
+            List<CustomerProps> list = new List<CustomerProps>();
+            DBDataReader reader = null;
+            CustomerProps props;
+
+            try
+            {
+                reader = RunProcedure("usp_CustomerSelectAll");
+                if (!reader.IsClosed)
+                {
+                    while (reader.Read())
+                    {
+                        props = new CustomerProps();
+                        props.SetState(reader);
+                        list.Add(props);
+                    }
+                }
+                return list;
+            }
+            catch (Exception e)
+            {
+                // log this exception
+                throw;
+            }
+            finally
+            {
+                if (!reader.IsClosed)
+                {
+                    reader.Close();
+                }
+            }
+        }
+
+        public bool Update(IBaseProps p)
+        {
+            int rowsAffected = 0;
+            CustomerProps props = (CustomerProps)p;
+
+            DBCommand command = new DBCommand();
+            command.CommandText = "usp_CustomerUpdate";
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("custID", DBDbType.Int32);
+            command.Parameters.Add("name", DBDbType.VarChar);
+            command.Parameters.Add("conCurrId", DBDbType.Int32);
+            command.Parameters["custID"].Value = props.CustomerID;
+            command.Parameters["name"].Value = props.Name;
+            command.Parameters["conCurrId"].Value = props.ConcurrencyID;
+
+            try
+            {
+                rowsAffected = RunNonQueryProcedure(command);
+                if (rowsAffected == 1)
+                {
+                    props.ConcurrencyID++;
+                    return true;
+                }
+                else
+                {
+                    string message = "Record cannot be updated. It has been edited by another user.";
+                    throw new Exception(message);
+                }
+            }
+            catch (Exception e)
+            {
+                // log this exception
+                throw;
+            }
+            finally
+            {
+                if (mConnection.State == ConnectionState.Open)
+                    mConnection.Close();
+            }
+        }
     }
 }
---
+++ MMABooksTests/CustomerDBTests.cs
@@ -0,0 +1,94 @@
+﻿using NUnit.Framework;
+
+using MMABooksProps;
+using MMABooksDB;
+
+using DBCommand = MySql.Data.MySqlClient.MySqlCommand;
+using System.Data;
+
+using System.Collections.Generic;
+using System;
+using MySql.Data.MySqlClient;
+using System.Runtime.Intrinsics.Arm;
+
+namespace MMABooksTests
+{
+    [TestFixture()]
+    internal class CustomerDBTests
+    {
+        CustomerDB db;
+
+        [SetUp]
+        public void ResetData()
+        {
+            db = new CustomerDB();
+            DBCommand command = new DBCommand();
+            command.CommandText = "usp_testingResetData";
+            command.CommandType = CommandType.StoredProcedure;
+            db.RunNonQueryProcedure(command);
+        }
+
+        [Test]
+        public void TestRetrieve()
+        {
+            CustomerProps p = (CustomerProps)db.Retrieve(1);
+            Assert.AreEqual("Birmingham", p.City);
+            Assert.AreEqual("Molunguri, A", p.Name);
+        }
+
+        [Test]
+        public void TestRetrieveAll()
+        {
+            List<CustomerProps> list = (List<CustomerProps>)db.RetrieveAll();
+            Assert.AreEqual(696, list.Count);
+        }
+
+        [Test]
+        public void TestCreate()
+        {
+            CustomerProps p = new CustomerProps();
+            p.Name = "Test Name";
+            p.Address = "101 Test Address";
+            p.City = "Testcity";
+            p.State = "OR";
+            //Remember to use valid (existing) statecode in testing 
+            p.Zipcode = "10001";
+            db.Create(p);
+            CustomerProps p2 = (CustomerProps)db.Retrieve(p.CustomerID);
+            Assert.AreEqual(p.GetState(), p2.GetState());
+
+            //Cleanup
+            db.Delete(p);
+        }
+
+        [Test]
+        public void TestDelete()
+        {
+            CustomerProps p = new CustomerProps();
+            p.Name = "Tobe Deleted";
+            p.Address = "101 Test Delete";
+            p.City = "Deletecity";
+            p.State = "OR";
+            p.Zipcode = "10001";
+            db.Create(p);
+
+            Assert.True(db.Delete(p));
+            Assert.Throws<Exception>(() => db.Retrieve(p.CustomerID));
+        }
+
+        [Test]
+        public void TestUpdate()
+        {
+            CustomerProps p = (CustomerProps)db.Retrieve(696);
+            string oldName = p.Name;
+            p.Name = "Doe, Jane";
+            Assert.True(db.Update(p));
+            p = (CustomerProps)db.Retrieve(696);
+            Assert.AreEqual("Doe, Jane", p.Name);
+
+            //Cleanup
+            p.Name = oldName;
+            db.Update(p);
+        }
+    }
+}   
